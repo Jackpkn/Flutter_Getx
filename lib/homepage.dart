cf28623c59d8465d989a3cce02c1e8be2eff0104
Pawan <jackpkn07@gmail.com>
2023-01-31 22:49:43 +0530

    getx added

---
+++ lib/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      width: 120,
+      height: 120,
+      child: const Text('jack and pawan'),
+    );
+  }
+}
